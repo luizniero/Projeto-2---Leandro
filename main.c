db9a04c68769151b34bfcef86a240492c389f26e
luizniero <luiz_niero@hotmail.com>
2017-10-04 17:55:01 -0300

    kdt
    Fazendo o cadastro.  Consistencia do banco ja esta pronta

--- main.c
+++ main.c
@@ -6,8 +6,6 @@
 //teste reverso
 
 int profundidadeglobal;
-const char hash[8] = "hash.txt";
-const char clientes[12] = "clientes.txt";
 
 
 /*typedef struct CLIENTE{
@@ -22,10 +20,10 @@ const char clientes[12] = "clientes.txt";
 int verifica_existencia(){
 	fflush(stdin);
 	printf("Verificando banco de dados...");
-	FILE *arquivo_hash = fopen(hash, "r");
-	FILE *arquivo_clientes = fopen(clientes, "r");
+	FILE *arquivo_hash = fopen("hash.txt", "r");
+	FILE *arquivo_clientes = fopen("clientes.txt", "r");
 	
-	if (arquivo_hash == NULL && arquivo_clientes == NULL){
+	if ((arquivo_hash == NULL) && (arquivo_clientes == NULL)){
 			printf("\nOs arquivos de banco nao foram encontrados.  Necessita criar um novo banco de dados....\n");
 			return 0;
 	}
@@ -38,18 +36,29 @@ int verifica_existencia(){
 			return 0;
 	}
 	printf("\nBanco de Dados Consistente.  Continuando execucao...\n");
+	fclose(arquivo_clientes);
+	fclose(arquivo_hash);
 	return 1; //Ambos os arquivos existem.
 }
 
 
 int criar_banco(){
 	printf("\nCriando banco de dados...");
-	FILE *arquivo_hash = fopen("hash.txt", "wb");
-	FILE *arquivo_clientes = fopen("clientes.txt", "wb");
+	FILE *arquivo_hash = fopen("hash.txt", "a");
+	FILE *arquivo_clientes = fopen("clientes.txt", "a");
 	if ((arquivo_hash != NULL) && (arquivo_clientes != NULL)){
 		fprintf(arquivo_hash, "2");
-		fprint(arquivo_hash, )
+		fprintf(arquivo_hash, "\n2,0"); //1 bucket
+		fprintf(arquivo_hash,"\n");
+		fprintf(arquivo_hash, "\n2,0"); //2 bucket
+		fprintf(arquivo_hash,"\n");
+		fprintf(arquivo_hash, "\n2,0"); //3 bucket
+		fprintf(arquivo_hash,"\n");
+		fprintf(arquivo_hash, "\n2,0"); //4 bucket
+		fprintf(arquivo_hash,"\n");
 		fclose(arquivo_hash);
+		//fprintf(arquivo_hash, "2,0");
+		//fclose(arquivo_hash);
 		printf("\nArquivos hash.txt e clientes.txt criados.  Continuando execucao...");
 		return 1;
 	}
@@ -62,40 +71,41 @@ int criar_banco(){
 
 
 
-void cadastra_cliente(FILE* arq,FILE* hash){
+void cadastra_cliente(FILE* arquivo_clientes,FILE* arquivo_hash){
 	int numlinha=0;
 	float numconta,contalim,contasaldo,conta,saldo,limite;
 	char nome[50],nomecliente[50];
-	
-	
-	
-	fscanf(hash,"%d",&profundidadeglobal);
-	//cliente* novo = (cliente*)malloc(sizeof(cliente));
-	printf("Digite o nome do cliente\n");
-	scanf("%s",nomecliente);
+	fscanf(arquivo_hash,"%d",&profundidadeglobal);
+
 	printf("Digite o numero da conta\n");
 	scanf("%f",&conta);
+	//aqui entra uma das funções de hash
+
+	printf("Digite o nome do cliente\n");
+	scanf("%s",nomecliente);
 	printf("Digite o limite do cliete\n");
 	scanf("%f",&limite);
 	printf("Digite o saldo inicial do cliente\n");
 	scanf("%f",&saldo);
-	rewind(arq);
-	while(!feof(arq)){
-	fscanf(arq,"%d||%s||%f||%f||%f",&numlinha,nome,&numconta,&contalim,&contasaldo);
+	rewind(arquivo_clientes);
+	while(!feof(arquivo_clientes)){
+	fscanf(arquivo_clientes,"%d|%s|%f|%f|%f",&numlinha,nome,&numconta,&contalim,&contasaldo);
+	printf("%d %s", numlinha, nome);
 	}
-	
-	fprintf(arq,"%d||%s||%.0f||%.2f||%.2f\n",numlinha++,nomecliente,conta,limite,saldo);
+	numlinha++;
+	fprintf(arquivo_clientes,"%d|%s|%.0f|%.2f|%.2f\n",numlinha,nomecliente,conta,limite,saldo);
+	printf("Registro escrito");
 }
 
 int main(){
-	int arquivos, bancos;
+	int arquivos, bancos, loop;
 	arquivos = verifica_existencia();
 	if (arquivos == 0){
 		bancos = criar_banco();
 		if(bancos == 0) return 0; //NÃ£o conseguiu criar os arquivos, encerra o programa.	
 	}
-	FILE* f=fopen("clients.txt","r+");
-	FILE* f2=fopen("hash.txt","r+");
+	FILE* arquivo_clientes=fopen("clientes.txt","r+");
+	FILE* arquivo_hash=fopen("hash.txt","r+");
 	int op=6;
 	while(op!=1 && op!=2 && op!=3 && op!=0){
 		printf("\nDigite sua opcao de uso.\n1-Cadastrar\n2-Remover\n3-Consultar por CPF\n4-Sair\n");
@@ -103,8 +113,8 @@ int main(){
 		if(op!=1 && op!=2 && op!=3 && op!=0)
 			printf("\nPor favor digite uma opcao valida.\n");
 		if(op==1){
-			op=6;
-			cadastra_cliente(f,f2);
+			cadastra_cliente(arquivo_clientes, arquivo_hash);
+			op=0;
 		}
 		if(op==2){
 			// chama removecliente
@@ -113,8 +123,8 @@ int main(){
 			// chama consultacliente
 		}
 		if(op==4){
-			fclose(f);
-			fclose(f2);
+			fclose(arquivo_clientes);
+			fclose(arquivo_hash);
 			exit(1);
 		}
 	}
